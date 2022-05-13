2f8c57fc5fa718081d677c7a72d271560acebd70
JZ <103975209+JingZ22@users.noreply.github.com>
2022-05-13 11:20:04 -0400

    Create def_func_practice.py

---
+++ def_func_practice.py
@@ -0,0 +1,14 @@
+#define a function to calculate the vavlue of x: a*x^2+b*x+c=0
+
+import math
+
+def quad(a,b,c)
+  x1 = (-b + math.sqrt(b^2-4*a*c))/(2*a)
+  x2 = (-b - math.sqrt(b^2-4*a*c))/(2*a)
+  return x1,x2
+
+x1, x2 = quad(1,2,3)
+print(x1, x2)
+
+
+  
